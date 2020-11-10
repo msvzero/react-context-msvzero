8a4ab7daaf3d25e52e26ef38f6dc9adf4b25cfc0
msvzero <msvzero@gmail.com>
2020-11-10 11:23:36 -0300

    Actualizacion de componente para usar useContext

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,27 +1,40 @@
-import React from 'react';
+import React , { useContext }from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 import { ThemeContext }  from '../contexts/ThemeContext';
 
-export default function ThemeToggle() {
-    return (
-        <AuthContext.Consumer>{(authContext) => (
-            <ThemeContext.Consumer>{(themeContext) => 
-                {
-                            const { toggleTheme } = themeContext;
-                            const { isAuthenticated, toggleAuth } = authContext;
-                            return(
-                             <div>
-                                 <p>Logeado: {isAuthenticated? 'true': 'false'}</p>
-                                 <button onClick={() => toggleTheme()}>Toggle the theme</button>{' '}
-                                 <button onClick={() => toggleAuth()}>Toggle Auth</button>
-                             </div>
-                            )
-                        }
-                    }
-                </ThemeContext.Consumer>
-                )
-            }
-        </AuthContext.Consumer>
+// export default function ThemeToggle() {
+//     return (
+//         <AuthContext.Consumer>{(authContext) => (
+//             <ThemeContext.Consumer>{(themeContext) => 
+//                 {
+//                             const { toggleTheme } = themeContext;
+//                             const { isAuthenticated, toggleAuth } = authContext;
+//                             return(
+//                              <div>
+//                                  <p>Logeado: {isAuthenticated? 'true': 'false'}</p>
+//                                  <button onClick={() => toggleTheme()}>Toggle the theme</button>{' '}
+//                                  <button onClick={() => toggleAuth()}>Toggle Auth</button>
+//                              </div>
+//                             )
+//                         }
+//                     }
+//                 </ThemeContext.Consumer>
+//                 )
+//             }
+//         </AuthContext.Consumer>
       
+//     )
+// }
+
+
+export default function ThemeToggle() {
+    const { toggleTheme } = useContext(ThemeContext);
+    const { isAuthenticated, toggleAuth } = useContext(AuthContext);
+    return (
+        <div>
+            <p>Logeado: {isAuthenticated? 'true': 'false'}</p>
+            <button onClick={() => toggleTheme()}>Toggle the theme</button>{' '}
+            <button onClick={() => toggleAuth()}>Toggle Auth</button>
+        </div>
     )
 }
